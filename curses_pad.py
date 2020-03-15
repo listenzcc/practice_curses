ccee0b56a9c73d14b54d4bbd38356d0caece4262
Chuncheng Zhang <listenzcc@mail.bnu.edu.cn>
2020-03-15 13:17:54 +0800

    Function improve.
    Improve curses_pad, now it is moving.

--- curses_pad.py
+++ curses_pad.py
@@ -1,18 +1,90 @@
 import curses
+import random
+
+pad_height = 100
+pad_width = 100
+
+window_topleft_y = 3
+window_topleft_x = 5
+window_bottomright_y = window_topleft_y + 15
+window_bottomright_x = window_topleft_x + 25
 
 screen = curses.initscr()
 
-pad = curses.newpad(100, 100)
-#  These loops fill the pad with letters; this is
-# explained in the next section
-for y in range(0, 100):
-    for x in range(0, 100):
+# X ruler
+x_rule = curses.newpad(1, 30)
+for x in range(26):
+    try:
+        x_rule.addch(0, x, ord('A') + x)
+    except curses.error:
+        pass
+x_rule.refresh(0, 0,
+               window_topleft_y-2, window_topleft_x,
+               window_topleft_y-2, window_topleft_x+25)
+
+# Y ruler
+y_rule = curses.newpad(30, 2)
+for y in range(26):
+    try:
+        s = str(y)
+        if len(s) == 1:
+            y_rule.addch(y, 0, ord(' '))
+            y_rule.addch(y, 1, ord(s[0]))
+        else:
+            y_rule.addch(y, 0, ord(s[0]))
+            y_rule.addch(y, 1, ord(s[1]))
+    except curses.error:
+        pass
+y_rule.refresh(0, 0,
+               window_topleft_y, window_topleft_x-4,
+               window_topleft_y+15, window_topleft_x-3)
+
+# Make the pad
+pad = curses.newpad(pad_height, pad_width)
+
+# Fill the pad
+for y in range(pad_height):
+    for x in range(pad_width):
         try:
-            pad.addch(y,x, ord('a') + (x*x+y*y) % 26)
+            pad.addch(y, x, ord('a') + random.randint(0, 25))
         except curses.error:
             pass
 
-#  Displays a section of the pad in the middle of the screen
-pad.refresh(0,0, 5,5, 20,75)
 
-print(pad.getkey())
+def refresh_pad(pad, pad_anchor_y, pad_anchor_x):
+    # Displays a section of the pad
+    pad.refresh(pad_anchor_y, pad_anchor_x,
+                window_topleft_y, window_topleft_x,
+                window_bottomright_y, window_bottomright_x)
+
+
+y, x = 0, 0
+refresh_pad(pad, y, x)
+
+for _ in range(1000000):
+    c = pad.getkey()
+
+    if c == 'q':
+        break
+
+    if c == 'l':
+        x += 1
+        if x > 10:
+            x = 10
+
+    if c == 'h':
+        x -= 1
+        if x < 0:
+            x = 0
+
+    if c == 'j':
+        y += 1
+        if y > 10:
+            y = 10
+
+    if c == 'k':
+        y -= 1
+        if y < 0:
+            y = 0
+
+    refresh_pad(pad, y, x)
